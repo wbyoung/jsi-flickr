62ef31b36a3fade8e2108e839e749e1f48d697cd
Whitney Young <wbyoung@fadingred.com>
2014-06-11 17:04:34 -0700

    Removed global vars.

--- application.js
+++ application.js
@@ -1,47 +1,52 @@
-var app = (function() {
-  var loading = false;
-  var pageNumber = 1;
+(function() {
+  'use strict';
 
-  var displayPhotos = function(photos) {
-    photos.forEach(function(photo) {
-      var $a = $('<a>')
-        .addClass('image')
-        .attr('href', photo.url)
-        .appendTo('#photos');
-      $('<p>').text(photo.title).appendTo($a);
-      $('<img>').attr('src', photo.thumbnailURL).appendTo($a);
-    });
-  };
+  var app = (function() {
+    var loading = false;
+    var pageNumber = 1;
 
-  var displayError = function() {
-    $('#photos img').remove();
-    $('#photos .error').show();
-  };
+    var displayPhotos = function(photos) {
+      photos.forEach(function(photo) {
+        var $a = $('<a>')
+          .addClass('image')
+          .attr('href', photo.url)
+          .appendTo('#photos');
+        $('<p>').text(photo.title).appendTo($a);
+        $('<img>').attr('src', photo.thumbnailURL).appendTo($a);
+      });
+    };
 
-  var loadPhotos = function() {
-    if (loading) { return; }
-    loading = true;
-    $.flickr.loadInteresting(pageNumber)
-    .then(function(photos, pageCount) {
-      if (pageNumber >= pageCount) {
+    var displayError = function() {
+      $('#photos img').remove();
+      $('#photos .error').show();
+    };
+
+    var loadPhotos = function() {
+      if (loading) { return; }
+      loading = true;
+      $.flickr.loadInteresting(pageNumber)
+      .then(function(photos, pageCount) {
+        if (pageNumber >= pageCount) {
+          $.infinite.remove('flickr-app');
+        }
+        pageNumber += 1;
+        loading = false;
+        displayPhotos(photos);
+      }, function(error) {
         $.infinite.remove('flickr-app');
+        loading = false;
+        displayError();
+      });
+    };
+
+    return {
+      run: function() {
+        loadPhotos();
+        $.infinite.add('flickr-app', loadPhotos);
       }
-      pageNumber += 1;
-      loading = false;
-      displayPhotos(photos);
-    }, function(error) {
-      $.infinite.remove('flickr-app');
-      loading = false;
-      displayError();
-    });
-  };
-
-  return {
-    run: function() {
-      loadPhotos();
-      $.infinite.add('flickr-app', loadPhotos);
     }
-  }
-})();
+  })();
 
-$(app.run);
+  $(app.run);
+
+})();
